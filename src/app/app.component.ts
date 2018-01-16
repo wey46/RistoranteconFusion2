8004026735d203b807c45c317959a077720e3356
wey46 <wyu0618@gmail.com>
2018-01-16 15:09:42 -0500

    Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //incline template instead of templateUrl: template: `<h1>{{title}}</h1>`
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
